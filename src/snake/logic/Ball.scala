26e79a3141297ea287c91f0c5322be0738b0b0c2
unknown <alexis.dragoste@gmail.com>
2022-10-24 00:19:37 +0200

    adding ball

--- src/snake/logic/Ball.scala
+++ src/snake/logic/Ball.scala
@@ -1,3 +1,11 @@
 package snake.logic
 
-case class Ball()
+case class Ball(position: Point, velocity: Point, gridDims: Dimensions){
+  def move(): Ball = Ball(position + velocity, velocity, gridDims)
+
+  def collidedWithHorizontalWall(): Boolean = false
+
+  def collidedWithVerticalPlayers(): Boolean = false
+
+  def collidedWithPLayer(player: Player): Boolean = false
+}
